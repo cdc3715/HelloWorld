b8742c06b299d2fe3017388bed137ed9915f7d5e
cdc3715 <464278398@qq.com>
2016-12-11 17:38:29 +0800

    2

--- src/com/HelloJava.java
+++ src/com/HelloJava.java
@@ -3,5 +3,6 @@ package com;
 public class HelloJava {
 	public static void main(String[] args) {
 		System.out.println("say helloJava RELEASE 1231 紧急修复呢 他是一幕");
+		System.out.println("say helloJava RELEASE 1231 紧急修复呢 他是一幕");
 	}
 }
