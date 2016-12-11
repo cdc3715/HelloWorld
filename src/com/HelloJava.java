66ab65088a64dc5e815c646623b38e2bd2c6fdb5
LBfile <513206491@qq.com>
2016-12-11 17:42:53 +0800

    3

--- src/com/HelloJava.java
+++ src/com/HelloJava.java
@@ -4,5 +4,6 @@ public class HelloJava {
 	public static void main(String[] args) {
 		System.out.println("say helloJava RELEASE 1231 紧急修复呢 他是一幕");
 		System.out.println("say helloJava RELEASE 1231 紧急修复呢 他是一幕");
+		System.out.println("say helloJava RELEASE 1231 紧急修复呢 他是一幕");		
 	}
 }
